a81022a7afe4e03452b346a4445facc426f5b16a
David Mihal <david@dmihal.com>
2018-12-28 22:26:08 +0700

    Service script actually updates the contract

--- service/index.js
+++ service/index.js
@@ -2,51 +2,59 @@ const fetch = require('node-fetch');
 const contract = require('truffle-contract');
 const Web3 = require('web3');
 
-const ExchangeRateBuild = require('../build/ExchangeRate.json');
+const ExchangeRateBuild = require('../build/contracts/ExchangeRate.json');
 
 const config = require('../truffle-config');
 
+const WEI = '1000000000000000000';
+
 const ExchangeRate = contract(ExchangeRateBuild);
-ExchangeRate.setProvider(config.networks.kovan.provider);
+//ExchangeRate.setProvider(config.networks.kovan.provider());
+ExchangeRate.setProvider(new Web3.providers.HttpProvider('http://localhost:8546'));
 
 const web3 = new Web3();
 
-let storedRate = 0;
-let lastUpdate = new Date(0);
-function setExchangeRate(newRate) {
-  console.log(`Updating price from $${storedRate} to $${newRate} (${percentDiff(storedRate, newRate)}%)`);
-  storedRate = newRate;
-  lastUpdate = new Date();
-}
-function getThreshold() {
-  let timeDiff = new Date() - lastUpdate;
-  return (60 * 60 * 1000) / timeDiff;
+function getThreshold(now, lastUpdate) {
+  let timeDiff = now - lastUpdate;
+  return (60 * 60) / timeDiff;
 }
 
 async function updateExchangeRate() {
   const response = await fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/');
   const data = await response.json();
   const currentRate = parseFloat(data[0].price_usd);
+  const currentRateInCents = Math.round(currentRate * 100);
   const timestamp = data[0].last_updated;
 
   const contract = await ExchangeRate.deployed();
-  const [storedRateInCents, lastUpdated] = await Promise.all([
+  const [storedRateInCents, lastUpdated] = (await Promise.all([
     contract.getExchangeRateInCents(),
-    contract.lastUpdated();
-  ]);
+    contract.lastUpdated(),
+  ])).map(bn => bn.toNumber());
 
-  const difference = percentDiff(currentRate * 100, storedRateInCents);
+  const difference = percentDiff(currentRateInCents, storedRateInCents);
   const threshold = getThreshold(timestamp, lastUpdated);
 
   if (difference > threshold) {
-    contract.setExchangeRate(timestamp);
+    console.log(`Updating price from $${storedRateInCents / 100} to $${currentRate} (${difference}%)`);
+    const newWeiPerCent = web3.utils.toBN(WEI).div(web3.utils.toBN(currentRateInCents));
+
+    const owner = await contract.owner();
+    const { receipt } = await contract.setExchangeRate(newWeiPerCent, timestamp, { from: owner });
+    console.log(`TX ${receipt.transactionHash} (block ${receipt.blockNumber})`);
   } else {
     console.log(`Skipping, $${currentRate} is ${difference}%, less than ${threshold}`)
   }
 }
 
 function percentDiff(a, b) {
-  return Math.abs((a - b) / ((a + b) / 2)) * 100;
+  const _a = web3.utils.toBN(a);
+  const _b = web3.utils.toBN(b);
+  const commonDenominator = web3.utils.toBN(10);
+
+  const top = _a.sub(_b).div(commonDenominator).toNumber();
+  const bottom = _a.add(_b).div(web3.utils.toBN(2)).div(commonDenominator).toNumber();
+  return Math.abs(top / bottom) * 100;
 }
 
 setInterval(updateExchangeRate, 60 * 1000);
