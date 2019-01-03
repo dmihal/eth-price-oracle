a5d41daff18bdcf4752bebe344304810cfcd1b5d
David Mihal <david@dmihal.com>
2019-01-03 14:06:00 +0700

    Set up additional testnets

--- truffle-config.js
+++ truffle-config.js
@@ -23,7 +23,7 @@
  */
 
 require('dotenv').config();
-const PrivateKeyProvider = require("truffle-privatekey-provider");
+const Web3 = require("web3");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
@@ -47,36 +47,32 @@ module.exports = {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8546,            // Standard Ethereum port (default: none)
-      //provider: () => new PrivateKeyProvider(process.env.PRIVATE_KEY, "http://localhost:8545"),
+      provider: () => new Web3.providers.WebsocketProvider("http://localhost:8546"),
       network_id: "*",       // Any network (default: none)
-    },
-
-    // Another network with more advanced options...
-    advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+      develop: true,
     },
 
     ropsten: {
-      provider: () => new PrivateKeyProvider(process.env.PRIVATE_KEY, "https://ropsten.infura.io/"),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/"),
       network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      gas: 500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
     kovan: {
-      provider: () => new PrivateKeyProvider(process.env.PRIVATE_KEY, "https://kovan.infura.io/"),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://kovan.infura.io/"),
       network_id: 42,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/"),
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     },
   },
 
@@ -88,7 +84,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.4.25",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
